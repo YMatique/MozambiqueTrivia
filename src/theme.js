52a5a277888eb3c48537eac11a8e5fd65019c1e6
Yuvi Matique <86082620+YMatique@users.noreply.github.com>
2025-03-13 15:14:30 +0200

    adding theme

---
+++ src/theme.js
@@ -0,0 +1,26 @@
+// src/themes.js
+export const themes = {
+    light: {
+      backgroundColor: '#f0f4f8',
+      textColor: '#2c3e50',
+      buttonColor: '#3498db',
+      accentColor: '#e74c3c',
+      secondaryTextColor: '#34495e',
+    },
+    dark: {
+      backgroundColor: '#2c3e50',
+      textColor: '#ecf0f1',
+      buttonColor: '#2980b9',
+      accentColor: '#c0392b',
+      secondaryTextColor: '#bdc3c7',
+    },
+    mozambique: {
+      backgroundColor: '#d35400', // Tom terroso
+      textColor: '#f1c40f', // Amarelo vibrante
+      buttonColor: '#27ae60', // Verde da bandeira
+      accentColor: '#e74c3c', // Vermelho
+      secondaryTextColor: '#f39c12',
+    },
+  };
+  
+  export const defaultTheme = 'light';
